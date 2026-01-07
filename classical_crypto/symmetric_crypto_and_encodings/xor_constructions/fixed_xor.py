a2fcced8ed5a350887482d68ed15401811be9536
AV - Security & Cryptography <250286626+av-crypto-security@users.noreply.github.com>
2026-01-07 22:33:05 +0300

    Update fixed_xor.py

--- classical_crypto/symmetric_crypto_and_encodings/xor_constructions/fixed_xor.py
+++ classical_crypto/symmetric_crypto_and_encodings/xor_constructions/fixed_xor.py
@@ -1,1 +1,10 @@
+def fixed_xor(hex1, hex2):
+    b1 = bytes.fromhex(hex1)
+    b2 = bytes.fromhex(hex2)
+    result = bytes(a ^ b for a, b in zip(b1, b2))
+    return result.hex()
 
+s1 = '1c0111001f010100061a024b53535009181c'
+s2 = '686974207468652062756c6c277320657965'
+
+print(fixed_xor(s1, s2))
