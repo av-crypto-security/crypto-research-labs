3e1a5bbcadbbc8dc329769076445d65d0ed7cade
AV - Security & Cryptography <250286626+av-crypto-security@users.noreply.github.com>
2026-01-07 22:34:04 +0300

    Update repeating_key_xor.py

--- classical_crypto/symmetric_crypto_and_encodings/xor_constructions/repeating_key_xor.py
+++ classical_crypto/symmetric_crypto_and_encodings/xor_constructions/repeating_key_xor.py
@@ -1,1 +1,29 @@
+# ---------------------------------------------------------- plaintext -> ciphertext -------------------------------------------------
+def repeating_key_xor(text: bytes, key: bytes) -> bytes:
+    result = bytearray()
+    for i in range(len(text)):
+        result.append(text[i] ^ key[i % len(key)])
+    return result
 
+plaintext = (
+    b"Burning 'em, if you ain't quick and nimble\n"
+    b"I go crazy when I hear a cymbal"
+)
+
+key = b'ICE'
+ciphertext = repeating_key_xor(plaintext, key)
+print(ciphertext.hex())
+
+# -------------------------------------------------------- ciphertext -> plaintext ---------------------------------------------------
+def repeating_key_xor(text: bytes, key: bytes) -> bytes:
+    result = bytearray()
+    for i in range(len(text)):
+        result.append(text[i] ^ key[i % len(key)])
+    return result
+
+hex_line = '''0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'''
+ciphertext = bytes.fromhex(hex_line)
+key = b'ICE'
+plaintext = repeating_key_xor(ciphertext, key)
+
+print(plaintext.decode())
